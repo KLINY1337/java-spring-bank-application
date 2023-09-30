ce11fa0ded1a7569a366c070e446fb62e54556d4
KLINY1337 <semyon.chernomurov@gmail.com>
2023-09-30 11:08:20 +0300

    Initial commit (configured dependencies and application.properties)

---
+++ src/test/java/com/example/demobank/DemoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
